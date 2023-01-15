154694c734974df9089cc1425a6e1c7bb58df851
afraj18 <afraj.2011@gmail.com>
2023-01-15 20:39:44 +0530

    Form Validation

--- src/main/java/com/myapp/hostel_management_system/HostelManagementSystemApplication.java
+++ src/main/java/com/myapp/hostel_management_system/HostelManagementSystemApplication.java
@@ -4,16 +4,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HostelManagementSystemApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HostelManagementSystemApplication.class, args);
+
+    @SpringBootApplication()
+    public class HostelManagementSystemApplication implements CommandLineRunner {
+
+
+        public static void main(String[] args) {
+            SpringApplication.run(HostelManagementSystemApplication.class, args);
+        }
+
+
+        @Override
+        public void run(String... args) throws Exception {
+
+        }
     }
-
-
-    @Override
-    public void run(String... args) throws Exception {
-
-    }
-}
